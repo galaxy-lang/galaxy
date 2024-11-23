cdcd78df0e035af73d38811082a209b00d53fe95
wesuRage <weslley.nmiranda@gmail.com>
2024-11-22 23:21:04 -0300

    bug(segfault): commiting for help

--- src/frontend/parser/print_ast.c
+++ src/frontend/parser/print_ast.c
@@ -1,24 +1,66 @@
 #include <stdio.h>
 #include "../../../include/ast/definitions.h"
 
+const char* returnASTNodeName(NodeType node_type) {
+    switch (node_type) {
+        case NODE_PROGRAM: return "Program";
+        case NODE_NUMERIC_LITERAL: return "Numeric Literal";
+        case NODE_IDENTIFIER: return "Identifier";
+        case NODE_BINARY_EXPR: return "Binary Expression";
+        default: return "Unknown";
+    }
+}
+
 void print_ast_node(const AstNode *node, int depth) {
     if (!node) return;
 
-    for (int i = 0; i < depth; i++) {
+    for (int i = 0; i < depth + 1; i++) {
         printf("  ");
     }
 
-    printf("Node Type: %d\n", node->kind);
+    printf("Node Type: %s\n", returnASTNodeName(node->kind));
 
     if (node->data) {
-        for (int i = 0; i < depth + 1; i++) {
-            printf("  ");
+        switch (node->kind) {
+            case NODE_PROGRAM:
+                printf("Value: (Program node does not have data)\n");
+                break;
+
+            case NODE_NUMERIC_LITERAL:
+                printf("Value: %f\n", ((NumericLiteralNode*)node->data)->value);
+                break;
+
+            case NODE_IDENTIFIER:
+                printf("Value: %s\n", ((IdentifierNode*)node->data)->symbol);
+                break;
+
+            case NODE_BINARY_EXPR:
+                {
+                    BinaryExprNode *binary_expr = (BinaryExprNode*)node->data;
+                    if (binary_expr) {
+                        printf("Left Operand: %p\n", binary_expr->left);
+                        printf("Right Operand: %p\n", binary_expr->right);
+                        printf("Operator: %s\n", binary_expr->operator);
+                    } else {
+                        printf("BinaryExprNode data is NULL\n");
+                    }
+                }
+                break;
+
+            default:
+                printf("Value: Unknown\n");
+                break;
         }
-        printf("Value: %s\n", node->data);
+    } else {
+        printf("Node data is NULL\n");
     }
 
-    for (size_t i = 0; i < node->child_count; i++) {
-        print_ast_node(node->children[i], depth + 1);
+    if (node->children) {
+        for (size_t i = 0; i < node->child_count; i++) {
+            print_ast_node(node->children[i], depth + 1);
+        }
+    } else {
+        printf("Node has no children or children pointer is NULL\n");
     }
 }
 
@@ -26,4 +68,3 @@ void print_ast(const AstNode *root) {
     printf("AST:\n");
     print_ast_node(root, 0);
 }
-
