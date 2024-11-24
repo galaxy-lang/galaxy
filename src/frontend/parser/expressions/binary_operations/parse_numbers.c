d4d2e3d21960b8471d9eaaea19f4a173e2527128
wesuRage <weslley.nmiranda@gmail.com>
2024-11-24 13:11:56 -0300

    fix(method): removed unused method

--- src/frontend/parser/expressions/binary_operations/parse_numbers.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include "../../../../../include/ast/definitions.h"
-#include "../../../../../include/ast/core.h"
-#include "../../../../../include/parser/core.h"
