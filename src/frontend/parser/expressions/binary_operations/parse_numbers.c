52b26e2bf9b5156b14a25a5c646c76e477dfe13b
Natan Scalvence <strelkauleb128@riseup.net>
2024-11-24 05:38:44 -0300

    (TODO):Recognize numbers in parser primary expressions

---
+++ src/frontend/parser/expressions/binary_operations/parse_numbers.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "../../../../../include/ast/definitions.h"
+#include "../../../../../include/ast/core.h"
+#include "../../../../../include/parser/core.h"
