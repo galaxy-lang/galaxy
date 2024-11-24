82e5814ee5c55f0e7cf5283550ac02f3d91d5453
wesuRage <weslley.nmiranda@gmail.com>
2024-11-23 21:43:40 -0300

    feat(docs): new code documentation

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/lexer/core.h"
+#include "../include/parser/core.h"
 #include "../include/utils.h"
 
+/**
+ * @brief Frees the memory allocated for an array of tokens.
+ *
+ * This function iterates over the array of tokens and frees the memory allocated for each token's lexeme and message.
+ * After freeing the memory for each token, it frees the memory for the array of tokens itself.
+ *
+ * @param tokens The array of tokens to be freed.
+ * @param tokenCount The number of tokens in the array.
+ */
 void freeTokens(Token *tokens, int tokenCount) {
     if (tokens != NULL) {
         for (int i = 0; i < tokenCount; i++) {
@@ -13,6 +23,16 @@ void freeTokens(Token *tokens, int tokenCount) {
     }
 }
 
+/**
+ * @brief Entry point for the program.
+ *
+ * Processes a source file by tokenizing its contents, generating an abstract syntax tree (AST), 
+ * and cleaning up allocated resources. 
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Command-line arguments: program name and source file path.
+ * @return 0 on success, or a non-zero error code on failure.
+ */
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: %s <source_file>\n", argv[0]);
@@ -27,7 +47,11 @@ int main(int argc, char **argv) {
 
     int count = 0;
     Token *tokens = tokenize(sourceFile, argv[1], &count);
+    
+    Parser parser = parser_new();
+    AstNode *ast = produce_ast(&parser, tokens, count);
 
+    free_ast_node(ast);
     freeTokens(tokens, count);
     fclose(sourceFile);
     return 0;
