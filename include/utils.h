def871583b5fdfbdfae20ea6bedc85f6bcfb385e
Natan Scalvence <strelkauleb128@riseup.net>
2024-12-03 20:27:36 -0300

    fix(gstrdup): Removed the gstrdup() functions in backend/ and added to utils.h as a macro.

--- include/utils.h
+++ include/utils.h
@@ -29,10 +29,45 @@
  *
  * On Windows, this macro maps strdup to _strdup, which is the Windows-specific
  * implementation. On other platforms, it leaves strdup unchanged.
+ * 
+ * OBS: For Windows compiler, the macro GSTRDUP_S may not work, due to conflicts
+ * to MSVC compiler. If so, use _strdup.
  */
+
 #ifdef _WIN32
 #define strdup _strdup
 #endif
+
+/**
+ * @brief Implements gstrdup(), a fork of strdup() for Galaxy Project.
+ * 
+ * This code implements a new more 'safer' version of strdup() for Galaxy Project.
+ * It's not a big deal and its quite simple, btw, theres a thousand variations of the same code
+ * around the web, so feel free to modify anything if u want.
+ * 
+ * @param String Returns the byte string pointer duplicated.
+ * @return copy The copied string
+ */
+
+#define MAX_GSTRDUP_STRING_LENGHT 1024 * 1024 // 1 Megabyte.
+
+#define STRDUP_S(string) ({                         \
+    size_t size;                                    \
+    char *copy;                                     \
+                                                    \
+    if (size > MAX_GSTRDUP_STRING_LENGHT) {         \
+        errno = EINVAL;                             \
+        return NULL;                                \
+    }                                               \
+                                                    \
+    size = strlen(string) + 1;                      \
+    if ((copy = malloc(size)) == NULL) {            \
+        return NULL;                                \
+    }                                               \
+                                                    \
+    (void)memcpy(copy, string, size);               \
+    copy;                                           \
+})                                                  
 
 /**
  * @brief Safely allocates memory and handles allocation failures.
