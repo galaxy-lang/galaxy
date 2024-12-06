031dd706d35ca4c8352711b34cfb63d2c16ad0b9
wesuRage <weslley.nmiranda@gmail.com>
2024-12-05 22:38:22 -0300

    feat(for): added for statement parsing

--- src/frontend/parser/statements/parse_for_stmt.c
+++ src/frontend/parser/statements/parse_for_stmt.c
@@ -3,106 +3,112 @@
 #include <string.h>
 #include "frontend/parser/statements/parse_for_stmt.h"
 #include "frontend/parser/statements/parse_stmt.h"
+#include "frontend/parser/expressions/parse_expr.h"
+#include "frontend/parser/expressions/binary_operations/parse_logical_expr.h"
+#include "frontend/parser/statements/parse_variable_declaration_stmt.h"
 #include "frontend/parser/types/parse_type.h"
 #include "frontend/parser/core.h"
 #include "frontend/types.h"
 #include "utils.h"
 
 AstNode *parse_for_stmt(Parser *parser) {
-	int line = at(parser).line;
-	int column_start = at(parser).column_start;
-	int position_start = at(parser).position_start;
+    int line = at(parser).line;
+    int column_start = at(parser).column_start;
+    int position_start = at(parser).position_start;
 
-	eat(parser);
- 	expect(parser, TOKEN_OPAREN, "Expected \"(\".");
+    eat(parser);
+    expect(parser, TOKEN_OPAREN, "Expected \"(\".");
 
-	Type iterable_type = parse_type(parser);
-	char *iterable = expect(parser, TOKEN_IDENTIFIER, "Expected identifier").lexeme;
+    Type var_type = TYPE_IMPLICIT;
+    bool var_isConst = false;
+    bool var_isPtr = false;
+    char *variable = NULL;
+    AstNode *iterator = NULL, *start = NULL, *stop = NULL, *updater = NULL;
 
-	char *start = NULL, *end = NULL, *action = NULL, *compare = NULL;
+    if (at(parser).type == TOKEN_CONST) {
+        var_isConst = true;
+        eat(parser);
+    }
 
-	if (at(parser).type == TOKEN_ASSIGN) {
-		eat(parser);
-		start = eat(parser).lexeme;
-		expect(parser, TOKEN_SEMICOLON, "Expected \";\".");
+    if (at(parser).type != TOKEN_IDENTIFIER) {
+        var_type = parse_type(parser);
+        if (at(parser).type == TOKEN_MUL) {
+            var_isPtr = true;
+            eat(parser);
+        }
+    }
 
-		if (strcmp(iterable, at(parser).lexeme) != 0) {
-			error(parser, "Iterable is different from the operand.");
-			exit(EXIT_FAILURE);
-		}
-		eat(parser);
+    variable = expect(parser, TOKEN_IDENTIFIER, "Expected variable identifier.").lexeme;
 
-		compare = eat(parser).lexeme;
-		end = eat(parser).lexeme;
-		expect(parser, TOKEN_SEMICOLON, "Expected \";\".");
+    if (at(parser).type == TOKEN_ASSIGN) {
+        eat(parser);
+        start = parse_expr(parser);
 
-		if (strcmp(iterable, at(parser).lexeme) != 0) {
-			error(parser, "Unexpected iterable.");
-			exit(EXIT_FAILURE);
-		}
-		eat(parser);
+        expect(parser, TOKEN_SEMICOLON, "Expected \";\".");
 
-		action = strdup(eat(parser).lexeme);
-	} else if (at(parser).type == TOKEN_COLON) {
-		eat(parser);
+        stop = parse_logical_expr(parser);
 
-		if (next(parser).type == TOKEN_RANGE || next(parser).type == TOKEN_IRANGE) {
-			start = eat(parser).lexeme;
-			eat(parser);
-			end = eat(parser).lexeme;
-		} else {
-			start = at(parser).lexeme;
-			end = at(parser).lexeme;
-			eat(parser);
-		}
-	} else {
-		error(parser, "Expected \":=\" or \":\".");
-		exit(EXIT_FAILURE);
-	}
+        expect(parser, TOKEN_SEMICOLON, "Expected \";\".");
 
-	expect(parser, TOKEN_CPAREN, "Expected \")\".");
-	expect(parser, TOKEN_COLON, "Expected \":\".");
+        updater = parse_expr(parser);
 
-	AstNode *iterable_data = create_iterable_data(
-		iterable,
-		iterable_type,
-		start,
-		end,
-		action,
-		compare
-	);
+    } else if (at(parser).type == TOKEN_COLON) {
+        eat(parser);
 
-	ForNode *for_data = MALLOC_S(sizeof(ForNode));
-	for_data->iterable = iterable_data;
-	for_data->body = NULL;
-	for_data->body_count = 0;
+        if (next(parser).type == TOKEN_RANGE || next(parser).type == TOKEN_IRANGE) {
+            start = parse_expr(parser);
+            eat(parser);
+            stop = parse_expr(parser);
+        } else {
+            iterator = parse_expr(parser);
+        }
 
-	while (not_eof(parser) && at(parser).type != TOKEN_END) {
-		for_data->body = REALLOC_S(
-			for_data->body,
-			sizeof(AstNode *) * (for_data->body_count + 1)
-		);
+    } else {
+        error(parser, "Unexpected token in for statement.");
+        exit(EXIT_FAILURE);
+    }
 
-		for_data->body[for_data->body_count] = parse_stmt(parser);
-		for_data->body_count++;
-	}
+    expect(parser, TOKEN_CPAREN, "Expected \")\".");
 
-	expect(parser, TOKEN_END, "Expected end.");
+    expect(parser, TOKEN_COLON, "Expected \":\".");
 
-	int column_end = at(parser).column_end - 1;
-	int position_end = at(parser).position_end - 1;
+    ForNode *for_data = MALLOC_S(sizeof(ForNode));
+    for_data->body = NULL;
+    for_data->body_count = 0;
+    for_data->variable = variable;
+    for_data->var_type = var_type;
+    for_data->var_isConst = var_isConst;
+    for_data->var_isPtr = var_isPtr;
+    for_data->start = start;
+    for_data->stop = stop;
+    for_data->updater = updater;
+    for_data->iterator = iterator;
 
-	expect(parser, TOKEN_SEMICOLON, "Expected \";\".");
+    while (not_eof(parser) && at(parser).type != TOKEN_END) {
+        for_data->body = REALLOC_S(
+            for_data->body,
+            sizeof(AstNode *) * (for_data->body_count + 1)
+        );
 
-	AstNode *for_node = create_ast_node(
-		NODE_FOR,
-		for_data,
-		line,
-		column_start,
-		position_start,
-		column_end,
-		position_end
-	);
+        for_data->body[for_data->body_count++] = parse_stmt(parser);
+    }
 
-	return for_node;
+    expect(parser, TOKEN_END, "Expected \"end\" to close for statement.");
+
+    int column_end = at(parser).column_end - 1;
+    int position_end = at(parser).position_end - 1;
+
+    expect(parser, TOKEN_SEMICOLON, "Expected \";\".");
+
+    AstNode *for_node = create_ast_node(
+        NODE_FOR,
+        for_data,
+        line,
+        column_start,
+        position_start,
+        column_end,
+        position_end
+    );
+
+    return for_node;
 }
