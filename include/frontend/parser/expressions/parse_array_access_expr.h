ce2e596e954d2894f358b6bf3039ebc1e0488eec
BrunoCiccarino <landneco0@gmail.com>
2024-12-07 16:43:58 -0300

    feat(parser): adding an parser array access expression

---
+++ include/frontend/parser/expressions/parse_array_access_expr.h
@@ -0,0 +1,8 @@
+#ifndef PARSE_ARRAY_ACCESS_EXPR_H
+#define PARSE_ARRAY_ACCESS_EXPR_H
+
+#include "frontend/parser/core.h"
+
+AstNode *parse_array_access_expr(Parser *parser, AstNode *expr);
+
+#endif // PARSE_ARRAY_ACCESS_EXPR_H
---
+++ src/frontend/parser/expressions/parse_array_access_expr.c
@@ -0,0 +1,5 @@
+#include "frontend/ast/definitions.h"
+#include "frontend/parser/expressions/parse_object_expr.h"
+#include "frontend/parser/expressions/parse_arguments_list.h"
+#include "frontend/parser/core.h"
+#include "utils.h" 
