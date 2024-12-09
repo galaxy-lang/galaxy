cf6006b7cd7ee4682853216aff810e43a2246755
wesuRage <weslley.nmiranda@gmail.com>
2024-12-09 20:44:10 -0300

    feat(Module): added Module to expressions

--- include/backend/generator/expressions/generate_unary_minus.hpp
+++ include/backend/generator/expressions/generate_unary_minus.hpp
@@ -7,6 +7,6 @@ extern "C" {
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 
-llvm::Value *generate_unary_minus(UnaryMinusExpr *node, llvm::LLVMContext &Context, llvm::IRBuilder<> &Builder);
+llvm::Value *generate_unary_minus(UnaryMinusExpr *node, llvm::LLVMContext &Context, llvm::IRBuilder<> &Builder, llvm::Module &Module);
 
 #endif // GENERATE_UNARY_MINUS_H
