360e12242365e9c799e2df3160c0a7ae5282ccb7
wesuRage <weslley.nmiranda@gmail.com>
2024-12-09 20:46:40 -0300

    feat(ternary): added ternary expr

---
+++ include/frontend/parser/expressions/parse_nested_ternary_expr.h
@@ -0,0 +1,8 @@
+#ifndef PARSE_NESTED_TERNARY_EXPR_H
+#define PARSE_NESTED_TERNARY_EXPR_H
+
+#include "frontend/parser/core.h"
+
+AstNode *parse_nested_ternary_expr(Parser *parser);
+
+#endif // PARSE_NESTED_TERNARY_EXPR_H
