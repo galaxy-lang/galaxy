5a728ffe9e418d8949f634e1b7a4af421de3b917
wesuRage <weslley.nmiranda@gmail.com>
2024-12-09 20:49:48 -0300

    feat(docs): better code generation documentation

--- src/backend/generator/generator.cpp
+++ src/backend/generator/generator.cpp
@@ -3,20 +3,23 @@
 #include "backend/generator/statements/generate_stmt.hpp"
 
 std::vector<llvm::Value*> generate_ir(AstNode *node, llvm::LLVMContext &Context, llvm::Module &Module, llvm::IRBuilder<> &Builder) {
-    if (node->kind != NODE_PROGRAM) {
-        throw std::runtime_error("Expected program node");
-    }
-
+    // Cast the AstNode to ProgramNode as the data in AstNode is assumed to be a ProgramNode
     ProgramNode *program = (ProgramNode *)node->data;
+
+    // Vector to hold the generated LLVM IR values
     std::vector<llvm::Value*> IRs;
 
+    // Iterate over all statements in the program and generate their corresponding IR
     for (size_t i = 0; i < program->statement_count; ++i) {
+        // Generate LLVM IR for each statement
         llvm::Value *IR = generate_stmt(program->statements[i], Context, Module, Builder);
         
+        // If the generated IR is not null, add it to the result vector
         if (IR) {
             IRs.push_back(IR);
         }
     }
 
+    // Return the vector containing the generated IR for the statements
     return IRs;
 }
