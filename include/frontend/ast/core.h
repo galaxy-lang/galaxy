5d8fa24f16bba684ebb8c31624918d652fb99171
wesuRage <weslley.nmiranda@gmail.com>
2024-12-09 13:54:45 -0300

    fix(types): now types are of type char *

--- include/frontend/ast/core.h
+++ include/frontend/ast/core.h
@@ -5,7 +5,6 @@
 #include <stdbool.h>
 #include "definitions.h"
 #include "frontend/lexer/core.h"
-#include "frontend/types.h"
 
 AstNode *create_ast_node(NodeType kind, void *data, int line, int column_start, int position_start, int column_end, int position_end);
 void add_child_to_node(AstNode *parent, AstNode *child);
@@ -14,8 +13,8 @@ void *create_identifier_data(const char *symbol);
 void *create_binary_expr_data(AstNode *left, AstNode *right, char *op);
 void *create_package_data(char *package);
 void *create_property_data(char *key, AstNode *value);
-void *create_variable_data(char *name, AstNode *value, bool isPtr, bool isConst, Type varType);
-void *create_param_data(char *name, Type type, bool isConst, bool isPtr);
+void *create_variable_data(char *name, AstNode *value, bool isPtr, bool isConst, char *varType);
+void *create_param_data(char *name, char *type, bool isConst, bool isPtr);
 void free_ast_node(AstNode *node);
 
 #endif // AST_H
