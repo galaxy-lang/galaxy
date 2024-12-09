e502a3ff9959d2a89f3e55e47737ee5252a3acf6
wesuRage <weslley.nmiranda@gmail.com>
2024-12-09 13:59:47 -0300

    feat(types): included new types generator path

--- src/backend/generator/statements/generate_function_declaration_stmt.cpp
+++ src/backend/generator/statements/generate_function_declaration_stmt.cpp
@@ -1,44 +1,28 @@
 #include "backend/generator/statements/generate_function_declaration_stmt.hpp"
 #include "backend/generator/statements/generate_stmt.hpp"
 #include "backend/generator/expressions/generate_expr.hpp"
+#include "backend/generator/types/generate_type.hpp"
 
 llvm::Value* generate_function_declaration_stmt(FunctionNode *node, llvm::LLVMContext &Context, llvm::Module &Module, llvm::IRBuilder<> &Builder) {
     if (!node || !node->name || !node->parameters) {
-        throw std::runtime_error("Função inválida: nó, nome ou parâmetros nulos.");
+        throw std::runtime_error("Invalid function: Node and/or Parameters is/are NULL.");
     }
 
-    // Determinar o tipo de retorno da função
-    llvm::Type *return_type;
-    switch (node->type) {
-        case TYPE_INT: return_type = llvm::Type::getInt32Ty(Context); break;
-        case TYPE_FLOAT: return_type = llvm::Type::getFloatTy(Context); break;
-        case TYPE_DOUBLE: return_type = llvm::Type::getDoubleTy(Context); break;
-        case TYPE_BOOL: return_type = llvm::Type::getInt1Ty(Context); break;
-        case TYPE_VOID: return_type = llvm::Type::getVoidTy(Context); break;
-        default: throw std::runtime_error("Tipo de retorno não suportado");
-    }
+    llvm::Type *return_type = generate_type(nullptr, Context, node->type);
 
-    // Determinar os tipos dos parâmetros
     std::vector<llvm::Type*> param_types;
     for (size_t i = 0; i < node->parameters->parameter_count; ++i) {
         AstNode *param_node = node->parameters->parameters[i];
         ParameterNode *param = static_cast<ParameterNode*>(param_node->data);
-        switch (param->type) {
-            case TYPE_INT: param_types.push_back(llvm::Type::getInt32Ty(Context)); break;
-            case TYPE_FLOAT: param_types.push_back(llvm::Type::getFloatTy(Context)); break;
-            case TYPE_DOUBLE: param_types.push_back(llvm::Type::getDoubleTy(Context)); break;
-            case TYPE_BOOL: param_types.push_back(llvm::Type::getInt1Ty(Context)); break;
-            default: throw std::runtime_error("Tipo de parâmetro não suportado");
-        }
+        
+        llvm::Type* param_type = generate_type(nullptr, Context, param->type);
+        param_types.push_back(param_type);
     }
 
-    // Criar o tipo da função
     llvm::FunctionType *func_type = llvm::FunctionType::get(return_type, param_types, false);
 
-    // Adicionar a função ao módulo
     llvm::Function *function = llvm::Function::Create(func_type, llvm::Function::ExternalLinkage, node->name, &Module);
 
-    // Definir os nomes dos parâmetros
     size_t idx = 0;
     for (auto &arg : function->args()) {
         AstNode *param_node = node->parameters->parameters[idx];
@@ -47,17 +31,14 @@ llvm::Value* generate_function_declaration_stmt(FunctionNode *node, llvm::LLVMCo
         ++idx;
     }
 
-    // Criar o corpo da função, se disponível
     if (node->body != nullptr) {
         llvm::BasicBlock *entry = llvm::BasicBlock::Create(Context, "entry", function);
         Builder.SetInsertPoint(entry);
 
-        // Processar o corpo da função
         for (size_t i = 0; i < node->body_count; ++i) {
             generate_stmt(node->body[i], Context, Module, Builder);
         }
 
-        // Adicionar retorno void se a função for do tipo void e não houver retorno explícito
         if (return_type->isVoidTy() && Builder.GetInsertBlock()->getTerminator() == nullptr) {
             Builder.CreateRetVoid();
         }
--- src/backend/generator/statements/generate_variable_declaration_stmt.cpp
+++ src/backend/generator/statements/generate_variable_declaration_stmt.cpp
@@ -1,21 +1,12 @@
 #include "backend/generator/statements/generate_variable_declaration_stmt.hpp"
 #include "backend/generator/expressions/generate_expr.hpp"
+#include "backend/generator/types/generate_type.hpp"
 
 llvm::Value* generate_variable_declaration_stmt(VariableNode *node, llvm::LLVMContext &Context, llvm::IRBuilder<> &Builder) {
-    // Determinar o tipo da variável
-    llvm::Type *var_type;
-    switch (node->varType) {
-        case TYPE_INT: var_type = llvm::Type::getInt32Ty(Context); break;
-        case TYPE_FLOAT: var_type = llvm::Type::getFloatTy(Context); break;
-        case TYPE_DOUBLE: var_type = llvm::Type::getDoubleTy(Context); break;
-        case TYPE_BOOL: var_type = llvm::Type::getInt1Ty(Context); break;
-        default: var_type = llvm::Type::getInt32Ty(Context); break;
-    }
+    llvm::Type *var_type = generate_type(nullptr, Context, node->varType);
 
-    // Criar espaço para a variável no IR
     llvm::AllocaInst *alloca = Builder.CreateAlloca(var_type, nullptr, node->name);
 
-    // Inicializar a variável, se um valor inicial for fornecido
     if (node->value != nullptr) {
         llvm::Value *init_value = generate_expr(node->value, Context, Builder);
         if (init_value->getType() != var_type) {
