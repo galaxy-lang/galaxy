f1f6ffcf0afa97df859bf04213ee76c22907a489
Natan Scalvence <strelkauleb128@riseup.net>
2024-12-14 14:59:16 -0300

    mod(function names): Function names changed Name of functions 'at', 'eat' and 'next' changed for better readability in the code.

--- src/frontend/parser/expressions/parse_logical_not_expr.c
+++ src/frontend/parser/expressions/parse_logical_not_expr.c
@@ -9,20 +9,20 @@
 #include "utils.h"
 
 AstNode *parse_logical_not_expr(Parser *parser) {
-  if (at(parser).type == TOKEN_NOT) {
-    int line = at(parser).line;
-    int column_start = at(parser).column_start;
-    int column_end = at(parser).column_end;
-    int position_start = at(parser).position_start;
-    int position_end = at(parser).position_end;
+  if (current_token(parser).type == TOKEN_NOT) {
+    int line = current_token(parser).line;
+    int column_start = current_token(parser).column_start;
+    int column_end = current_token(parser).column_end;
+    int position_start = current_token(parser).position_start;
+    int position_end = current_token(parser).position_end;
 
     AstNode *operand = parse_logical_not_expr(parser);
 
     LogicalNotExpr *logical_not_data = MALLOC_S(sizeof(LogicalNotExpr));
     logical_not_data->op = operand;
 
-    column_end = at(parser).column_end - 1;
-    position_end = at(parser).position_end - 1;
+    column_end = current_token(parser).column_end - 1;
+    position_end = current_token(parser).position_end - 1;
 
     AstNode *logical_not_node = create_ast_node(
       NODE_LOGICAL_NOT,
