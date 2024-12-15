1b3398781e2731208d16aaf8fe80d1ab38ea7798
Natan Scalvence <strelkauleb128@riseup.net>
2024-12-15 14:13:47 -0300

    feat(parse_if_stmt): Created the TOKEN for IF in the parse_stmt functions.

--- src/frontend/parser/statements/parse_stmt.c
+++ src/frontend/parser/statements/parse_stmt.c
@@ -11,6 +11,7 @@
 #include "frontend/parser/statements/parse_function_declaration_stmt.h"
 #include "frontend/parser/statements/parse_decorator_stmt.h"
 #include "frontend/parser/statements/parse_for_stmt.h"
+#include "frontend/parser/statements/parse_if_stmt.h"
 #include "frontend/parser/statements/parse_extern_stmt.h"
 #include "frontend/parser/types/parse_type.h"
 
@@ -19,6 +20,7 @@ AstNode *parse_stmt(Parser *parser) {
         case TOKEN_PACKAGE: return parse_package_stmt(parser);
         case TOKEN_IMPORT: return parse_import_stmt(parser);
         case TOKEN_FOR: return parse_for_stmt(parser);
+        case TOKEN_IF: return parse_if_stmt(parser);
         case TOKEN_DEF: return parse_function_declaration_stmt(parser);
         case TOKEN_AT: return parse_decorator_stmt(parser);
         case TOKEN_EXTERN: return parse_extern_stmt(parser);
