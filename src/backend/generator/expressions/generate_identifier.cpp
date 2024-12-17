bcffddaaeb7823afdee711aaeb09fb6c58790bbb
wesuRage <weslley.nmiranda@gmail.com>
2024-12-16 21:20:33 -0300

    feat(SymbolInfo): return only the llvm::value of identifier

--- src/backend/generator/expressions/generate_identifier.cpp
+++ src/backend/generator/expressions/generate_identifier.cpp
@@ -2,14 +2,11 @@
 #include "backend/generator/symbols/identifier_symbol_table.hpp"
 
 llvm::Value *generate_identifier(IdentifierNode *node) {
-    // Usa find_identifier para buscar o valor associado ao símbolo no escopo atual
-    llvm::Value *value = find_identifier(node->symbol);
+    const SymbolInfo *id = find_identifier(node->symbol);
 
-    // Verifica se o identificador foi encontrado
-    if (!value) {
+    if (!id) {
         throw std::runtime_error("Error: identifier not found!");
     }
 
-    // Retorna o LLVM Value associado ao identificador
-    return value;
+    return id->value;
 }
