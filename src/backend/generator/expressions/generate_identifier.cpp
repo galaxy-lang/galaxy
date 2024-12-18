ed77e80f625ea9f37b579f9ea1d1f79f49f6a219
wesuRage <weslley.nmiranda@gmail.com>
2024-12-17 21:25:47 -0300

    feat(parallel): parallel queue for generating identifiers

--- src/backend/generator/expressions/generate_identifier.cpp
+++ src/backend/generator/expressions/generate_identifier.cpp
@@ -1,12 +1,12 @@
 #include "backend/generator/expressions/generate_identifier.hpp"
 #include "backend/generator/symbols/identifier_symbol_table.hpp"
+#include "backend/generator/parallel/queue.hpp"
 
 llvm::Value *generate_identifier(IdentifierNode *node) {
-    const SymbolInfo *id = find_identifier(node->symbol);
-
-    if (!id) {
+    try {
+        return find_or_wait_for_identifier(node);
+    } catch (...) {
         throw std::runtime_error("Error: identifier not found!");
     }
-
-    return id->value;
 }
+
