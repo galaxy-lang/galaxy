314a2f5f2c6a7cb3bf7d1ecc40f42ace5f60e621
wesuRage <weslley.nmiranda@gmail.com>
2024-12-17 21:21:17 -0300

    feat(checker): changing to C++

---
+++ src/frontend/checker/checker.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+extern "C" {
+  #include "frontend/checker/core.h"
+  #include "frontend/checker/common_types.h"
+  #include "utils.h"
+}
+
+Checker* init(AstNode* ast) {
+
+}
