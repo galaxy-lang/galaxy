1968ff6a50cb3ab5c47c29c335f1e2e30f70eb15
wesuRage <weslley.nmiranda@gmail.com>
2024-12-19 21:44:51 -0300

    fix(operator): now operator has the right type

--- src/backend/generator/expressions/generate_pre_increment.cpp
+++ src/backend/generator/expressions/generate_pre_increment.cpp
@@ -4,6 +4,8 @@
 llvm::Value *generate_pre_increment(PreIncrementExpr *node, llvm::LLVMContext &Context, llvm::IRBuilder<> &Builder, llvm::Module &Module) {
     // Generate LLVM IR for the operand (the value to be incremented)
     llvm::Value *Operand = generate_expr(node->op, Context, Builder, Module);
+
+    Operand = Builder.CreatePtrToInt(Operand, llvm::Type::getInt32Ty(Context));
     
     // Create an addition instruction to increment the operand by 1
     // This is a pre-increment operation, so the operand is increased before returning the result.
