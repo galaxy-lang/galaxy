1d3d2fe5a43100c1d989fba814fac6b9880db346
wesuRage <weslley.nmiranda@gmail.com>
2025-01-11 15:24:46 -0300

    fix(futures): removed futures from it

--- src/backend/generator/symbols/identifier_symbol_table.cpp
+++ src/backend/generator/symbols/identifier_symbol_table.cpp
@@ -1,21 +1,28 @@
 #include "backend/generator/symbols/identifier_symbol_table.hpp"
 #include "backend/generator/symbols/symbol_stack.hpp"
+#include <stdexcept>
 
-const SymbolInfo *find_identifier(const std::string &name) {
-    std::lock_guard<std::mutex> lock(symbol_stack_mutex);
+// Função para encontrar o identificador na pilha de símbolos
+SymbolInfo *find_identifier(const std::string &name) {
+    if (symbol_stack.empty()) {
+        throw std::runtime_error("Symbol stack is empty. Cannot find identifier.");
+    }
 
-    for (const auto& entry : symbol_stack.top()) {
+    for (auto& entry : symbol_stack.top()) {
         if (entry.first == name) {
             return &entry.second;
         }
     }
-    
+
     return nullptr;
 }
 
+// Função para adicionar um identificador à pilha de símbolos
 void add_identifier(const std::string& name, llvm::Value* declaration, llvm::Value* value, llvm::Type* type) {
     if (symbol_stack.empty()) {
         throw std::runtime_error("No active scope to add identifier.");
     }
+
+    // Adiciona o identificador à pilha de símbolos
     symbol_stack.top()[name] = SymbolInfo{declaration, value, type};
 }
