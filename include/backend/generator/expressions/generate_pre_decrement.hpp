a224951369d232190c788fb3d16384511f8abe74
wesuRage <weslley.nmiranda@gmail.com>
2025-01-11 20:58:14 -0300

    fix(optim): removed optimization

--- include/backend/generator/expressions/generate_pre_decrement.hpp
+++ include/backend/generator/expressions/generate_pre_decrement.hpp
@@ -7,7 +7,6 @@ extern "C" {
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/NoFolder.h>
-#include <llvm/IR/NoFolder.h>
 
 llvm::Value *generate_pre_decrement(PreDecrementExpr *node, llvm::LLVMContext &Context, llvm::IRBuilder<llvm::NoFolder> &Builder, llvm::Module &Module);
 
