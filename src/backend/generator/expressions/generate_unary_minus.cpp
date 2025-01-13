7e00d3d8de8193340b04ea6342a03d26b2e450d3
wesuRage <weslley.nmiranda@gmail.com>
2025-01-13 02:39:08 -0300

    fix(double): added floating point case

--- src/backend/generator/expressions/generate_unary_minus.cpp
+++ src/backend/generator/expressions/generate_unary_minus.cpp
@@ -5,9 +5,9 @@ llvm::Value *generate_unary_minus(UnaryMinusExpr *node, llvm::LLVMContext &Conte
     // Generate LLVM IR for the operand (the value to apply the unary minus operation on)
     llvm::Value *Operand = generate_expr(node->op, Context, Builder, Module);
     
-    // Create a floating-point negation instruction, which negates the operand (i.e., multiplies it by -1)
-    llvm::Value *Negated = Builder.CreateNeg(Operand, "negtmp");
-    
-    // Return the result of the negation
-    return Negated;
+    if (Operand->getType()->isDoubleTy()) {
+        return Builder.CreateFNeg(Operand, "negtmp");
+    } else {
+        return Builder.CreateNeg(Operand, "negtmp");
+    }
 }
