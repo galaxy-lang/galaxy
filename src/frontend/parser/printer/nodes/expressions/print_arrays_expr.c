74ebdd3e1d7284cca2d5fcf27eb13efbdca71a50
Natan Scalvence <strelkauleb128@riseup.net>
2025-01-22 22:21:57 -0300

    feat(array): Added the arrays expressions (not 100% ready).

---
+++ src/frontend/parser/printer/nodes/expressions/print_arrays_expr.c
@@ -0,0 +1,31 @@
+#include "frontend/parser/printer/nodes/expressions/print_arrays_expr.h"
+#include "frontend/parser/printer/visited.h"
+#include "frontend/ast/definitions.h"
+#include "frontend/parser/printer/print_indent.h"
+#include "frontend/parser/printer/print_ast.h"
+
+void print_arrays_expr(const AstNode *node, int depth, VisitedNodes *visited) {
+    ArrayNode *array_data = (ArrayNode *)node->data;
+
+    if (array_data) {
+        print_indent(depth + 1);
+        printf("Array elements: \n");
+
+        if (array_data->element_count > 0) {
+            for (size_t i = 0; i < array_data->element_count; i++) {
+                print_indent(depth + 2);
+                printf("Element %zu\n", i);
+
+                if (array_data->elements[i]) {
+                    print_ast_node(array_data->elements[i], depth + 3, visited);
+                } else {
+                    print_indent(depth + 3);
+                    printf("Element is NULL\n");
+                }
+            }
+        }
+    } else {
+        print_indent(depth + 1);
+        printf("Array data is NULL\n");
+    }
+}
