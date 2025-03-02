b5f0215c0dcb50b05c73afa33d594d057d3e6bab
wesuRage <weslley.nmiranda@gmail.com>
2025-03-02 02:58:43 -0300

    feat(array): printing array

--- include/frontend/parser/printer/nodes/expressions/print_array.h
+++ include/frontend/parser/printer/nodes/expressions/print_array.h
@@ -1,9 +1,9 @@
-#ifndef PRINT_ARRAYS_H
-#define PRINT_ARRAYS_H
+#ifndef PRINT_ARRAY_H
+#define PRINT_ARRAY_H
 
 #include "frontend/ast/definitions.h"
 #include "frontend/parser/printer/visited.h"
 
 void print_array(const AstNode *node, int depth, VisitedNodes *visited);
 
-#endif // PRINT_ARRAYS_H
+#endif // PRINT_ARRAY_H
